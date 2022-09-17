9e4408494f4d6bd41c6c7d5cb331950832f6469b
Balaji Malali <82820381+balajimalali@users.noreply.github.com>
2022-09-17 18:05:37 +0530

    Update settings.py

--- tedx/settings.py
+++ tedx/settings.py
@@ -142,8 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'tedx/static'),
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'tedx/static')
 ]
 
 
