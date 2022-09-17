8b4e98e377f450f7cd31f6549a612508e9090d4e
Balaji Malali <82820381+balajimalali@users.noreply.github.com>
2022-09-17 17:52:00 +0530

    Update settings.py

--- tedx/settings.py
+++ tedx/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,17 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&+wc*#6!qxma02x9dwlcopo_pxj$$atiu1jxs+&^318r#g7@fj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-HOST = 'http://127.0.0.1:8000'
-SALT = '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW'
-KEYP = 'gtKFFx'
-TXNSALT = 'uhfgbtjhshbb'
+HOST = 'https://tedxnitw.herokuapp.com'
+SALT = os.environ.get('SALT')
+KEYP = os.environ.get('KEYP')
+TXNSALT = os.environ.get('TXNSALT')
 
 ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost',
     '127.0.0.1',
     HOST
 ]
@@ -47,10 +51,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'payments.apps.PaymentsConfig',
     'tedx',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,6 +85,8 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 WSGI_APPLICATION = 'tedx.wsgi.application'
 
 
@@ -92,6 +100,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -127,7 +137,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+#location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
